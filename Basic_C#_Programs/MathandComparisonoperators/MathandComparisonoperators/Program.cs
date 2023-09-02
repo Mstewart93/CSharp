86a495cf048ff969d01c632ee59982aa36de159f
Mstewart93 <melyssaannestewart@gmail.com>
2023-09-02 10:58:35 -0500

    Learning number operations

---
+++ Basic_C#_Programs/MathandComparisonoperators/MathandComparisonoperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathandComparisonoperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int total = 5 + 7;
+            //Console.WriteLine("5 + 7 = " + total.ToString() );
+
+        }
+    }
+}
