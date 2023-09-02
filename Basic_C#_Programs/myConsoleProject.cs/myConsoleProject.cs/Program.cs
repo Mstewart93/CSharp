e8d44ee2c5e7ddadd6c3a4141bb6dfd96514a220
Mstewart93 <melyssaannestewart@gmail.com>
2023-09-02 08:25:36 -0500

    ConsoleApp
    Created  console app that takes and displays user input.

--- Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
 
-namespace myConsoleProject.cs
+
+namespace myConsoleProject.cs 
+/*Name Space is an added layer of organization. It is listed above
+ class, this helps us remember where items are stored. For now it is the 
+same as program.*/
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        /*static void Main(string[] args) is a function named main that tkes on the argument args.
+         Reminder Subprogram will need parameters or data, and the acutal data is 
+        an argument.*/
         {
+            Console.WriteLine("What is your name?");//will display
+            string name = Console.ReadLine(); //takes input
+            Console.WriteLine("Hello, " + name + "!"); //Will insert name vsrible into string
+            Console.Read(); // prevents console auto closing, makes it read then display
+
+
+
         }
     }
 }
