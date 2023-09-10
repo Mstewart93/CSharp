72de7ccfa455cf872b1582e7ab3414b397526db5
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-09-10 12:05:40 -0600

    added comments

--- Package/Package/Program.cs
+++ Package/Package/Program.cs
@@ -13,23 +13,23 @@ namespace Insurance
         {
         Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
         Console.WriteLine("Please enter the package weight: ");
-        string weight = Console.ReadLine();
+        string weight = Console.ReadLine(); // saves the weight as variable to be used later
             
 
-        if (Convert.ToInt32(weight) < 50) {
-                Console.WriteLine("Please enter the package width: ");
+        if (Convert.ToInt32(weight) < 50) { // checks weight fits into parameters 
+                Console.WriteLine("Please enter the package width: "); //weight is fine so it takes different measurments W, H, L
                 string width = Console.ReadLine();
                 Console.WriteLine("Please enter the height: ");
                 string height = Console.ReadLine();
                 Console.WriteLine("Please enter the length: ");
                 string length = Console.ReadLine();
-                 if (Convert.ToInt32(width) + Convert.ToInt32(height) + Convert.ToInt32(length) < 50  ) {
+                 if (Convert.ToInt32(width) + Convert.ToInt32(height) + Convert.ToInt32(length) < 50  ) { // if the dimensions added are not too big we get shipping total
                     double shipping = (((Convert.ToInt32(width) * Convert.ToInt32(height) *Convert.ToInt32(length)) * Convert.ToInt32(weight)) / 100);
                     Console.WriteLine("Your estimated total for shipping this package is: " + shipping);
                  }
                  else {
-                    Console.WriteLine("Package too big to be shipped via Package Express");}
-        else {
+                    Console.WriteLine("Package too big to be shipped via Package Express");} //if dimensions are too big it kicks it out.
+        else { //if weight is to heavy kicks it here and ends it 
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
         
