1e9a927feec5177b1a7c6e1b46d18555a83015b6
Mstewart93 <melyssaannestewart@gmail.com>
2023-09-10 12:15:11 -0500

    Fized Anual Salary

--- Basic_C#_Programs/AnonymousIncome/AnonymousIncome/Program.cs
+++ Basic_C#_Programs/AnonymousIncome/AnonymousIncome/Program.cs
@@ -15,21 +15,21 @@ namespace AnonymousIncome
             Console.WriteLine("Person 1:"); 
             Console.WriteLine("Hourly Rate");
             string rate1 = Console.ReadLine(); //Takes input for person 1 hourly rate
-            Double Hrate1 = Convert.ToDouble(rate1);//Converst to something we can do math with
+            
             Console.WriteLine("Hours Worked Per Week: ");
             string week1 = Console.ReadLine();//Takes hours per week input
-            int Hweek1 = Convert.ToInt32(week1);//Converst so we cn do mth
+   
             Console.WriteLine("Person 2:");
             Console.WriteLine("Hourly Rate");
             string rate2 = Console.ReadLine();//Takes input for person 2 hourly rate
-            Double Hrate2 = Convert.ToDouble(rate2);//Converst to something we can do math with
+           
             Console.WriteLine("Hours Worked Per Week: ");
             string week2 = Console.ReadLine();//Takes hours per week input
-            int Hweek2 = Convert.ToInt32(week2);//Converst so we cn do mth
-            double AnnualSalary1 = Hrate1 * Hweek1 * 52; //Creats the sallary to compare by
-            double AnnualSalary2 = Hrate2 * Hweek2 * 52;
-            Console.WriteLine("Annual salary of Person 1:\n" + AnnualSalary1); //Prints the salary of both
-            Console.WriteLine("Annual salary of Person 2:\n" + AnnualSalary2);
+           
+            int Salary = Convert.ToInt32(rate1) * Convert.ToInt32(week1) * 52; //Creats the sallary to compare by
+            int Salary2 = Convert.ToInt32(rate2) * Convert.ToInt32(week2) * 52;
+            Console.WriteLine("Annual salary of Person 1:\n" + Salary); //Prints the salary of both
+            Console.WriteLine("Annual salary of Person 2:\n" + Salary2);
             Console.WriteLine("Does Person 1 make more money than Person 2?"); 
             bool turefalse = AnnualSalary1 > AnnualSalary2; //Makes the coprison
             Console.WriteLine(turefalse); //prints the comprison
