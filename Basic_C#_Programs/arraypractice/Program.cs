647339096acfdb466cab2244a5c4a07c994f36af
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-11-09 16:22:39 -0700

    Update Program.cs

--- Basic_C#_Programs/arraypractice/Program.cs
+++ Basic_C#_Programs/arraypractice/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace arraypractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+//Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
+//Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
+//Add in a message that displays when the user selects an index that doesn’t exist.
+//Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
+//Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code. 
+
+
         }
     }
 }
