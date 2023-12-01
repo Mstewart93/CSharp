5122d8a97cabe53adc211f47f8d060f77ef79bca
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-12-01 08:58:52 -0700

    Update Program.cs

--- Basic_C#_Programs/6partassignment/Program.cs
+++ Basic_C#_Programs/6partassignment/Program.cs
@@ -10,26 +10,34 @@ namespace _6partassignment
     {
          Main()
         {
-            // Assignment part one Create a console app that does/contains the following:
-            //1 A one-dimensional array of strings., 2 Ask the user to input some text. 3 A loop that iterates through each string in the array and 
+            // Assignment part one
+
             //adds the user's text input to the end of each string. This step will not output anything to the console, 
             //but will update each array element by appending the user's text. 4Then create a second loop that prints off each string in the array one at a time.
-            //Save and execute your code.
 
-            Console.WriteLine();
+            string[] str1;
+            str1 = new string[5]{"Dogs","Chickens","Cats","Ducks","Cows"};
+            Console.WriteLine("Please enter your favorite animal bellow:  ");
+            string useranimal = Console.Readline;
+            foreach (string i in str1) 
+            {
+                Console.WriteLine(i);
+
+            }
 
             //Assignment Part 2 
-           // Add the following to your console app and perform these actions: An infinite loop. Save your code. Fix the infinite loop so that it will execute properly.
-        //Add comments to your code indicating how the infinite loop was fixed. Save and execute your code.
+           // Add the following to your console app and perform these actions: 
+           //An infinite loop. Save your code. 
+           //Fix the infinite loop so that it will execute properly.
+        //Add comments to your code indicating how the infinite loop was fixed. 
 
         Console.WriteLine();
 
         //Assignment part 3
         //Add the following to your console app and perform these actions:
         //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
-        //Save and execute your code.
         //Add another loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
-        //Save and execute your code.
+
 
         Console.WriteLine();
 
@@ -40,7 +48,7 @@ namespace _6partassignment
         //A loop that iterates through the list and then displays the index of the list item that contains matching text on the screen.
         //Add code to check if the user put in text that isn't on the list and, if they did, tell the user their input is not on the list. 
         //Add code that stops the loop from executing once a match has been found.
-        //Save and execute your code.
+    
 
         Console.WriteLine();
 
@@ -49,7 +57,7 @@ namespace _6partassignment
         //A list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list.
         //Create a loop that iterates through the list and then displays the indices of the items matching the user-selected text. Ensure to remove any break statements that may prevent your code from returning multiple matches.
         //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list. (You do not need to provide any additional chances for the user to enter text.)
-        //Save and execute your code.
+
 
         Console.WriteLine();
         //Assignment Part 6
@@ -61,7 +69,6 @@ namespace _6partassignment
         //C - this item is unique
         //D - this item is unique
         //C - this item is a duplicate
-        //Save and execute your code.
         //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
         //Upload all the code you wrote during every step of this assignment to your GitHub account and submit the link in the space below:
 
