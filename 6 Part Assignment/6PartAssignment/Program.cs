194efb6b42db079db7111f96456e11642acc2b30
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2024-01-07 06:37:16 -0700

    working on 6 part assignment
    Trying to work through issues and bugs, to finish the assignment.

--- 6 Part Assignment/6PartAssignment/Program.cs
+++ 6 Part Assignment/6PartAssignment/Program.cs
@@ -17,11 +17,13 @@ namespace _6partassignment
             Console.WriteLine("Please enter your favorite animal bellow:  ");
             string useranimal = Console.ReadLine();
             for (int i = 0; i < str1.Length; i++)
-{
+                {
                 str1[i] = str1[i] + useranimal;
-}
-            for (int i in str1; i++)
-            {Console.WriteLine(str1 + useranimal)};
+                }
+            for (int i = 0;i < useranimal.Length; i++)
+            {
+                Console.WriteLine(str1 + useranimal);
+            }
 
 
 
@@ -35,8 +37,8 @@ namespace _6partassignment
         //y = Console.ReadLine();
         //Console.WriteLine()"Hello! Welcome";
         //Since x will always equal 1 this will run constantly we need to change it to one that has an end
-     
-        while (int x = 5;,x > 0, x--){
+        int[] x = 5;
+        while (x > 0; x--){
             Console.WriteLine("Please Enter Your name ");
             Console.ReadLine(); 
             Console.WriteLine("Welcome!");
@@ -48,72 +50,72 @@ namespace _6partassignment
         
         //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
         //Add another loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
-        for (int i = 0; i < 10; i++)
-{
+        for (int j = 0; j < 10; j++)
+            {
             Console.WriteLine("Hello, this should post 9 times :)");
-}       
-        while (int i = 1; i <= 10; i++)
-            {Console.WriteLine("This should post 10 times however")};
+            }       
+        while (int k = 1; k <= 10; k++)
+            {Console.WriteLine("This should post 10 times however");}
 
         //Assignment Part 4
         
         
         //A list of strings where each item in the list is unique.
-        string []str2;
-        str2 = ['mouse','horse','leopard','zebra','orangatang']
+        string[] str2 = {'mouse','horse','leopard','zebra','orangatang'}
         
-
-        for (int i in str2; i++)
-            {Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
-            string userinput = Console.ReadLine().ToLower;
-            bool alreadyexists = str2.Contains(userinput)};
-                if (alreadyexists = true):
-                    {Console.WriteLine("");
-
-                    }
+        {Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
+        string[] userinput = Console.ReadLine().ToLower;
+        
+        for (int m = 0; m>str2.Length; m++)
+            {
+                if (str2[m] == userinput)
+                {Console.WriteLine("Your Favorite Animal is on the list!" + userinput.IndexOf(userinput));
+                }
+            
                 else
                 {
-                    
+            Console.WriteLine( "Your Animal is not on the list");
                 }
-                    
-                
-            );
-
+            }
         
-
-
-        //A loop that iterates through the list and then displays the index of the list item that contains matching text on the screen.
-
-        //Add code to check if the user put in text that isn't on the list and, if they did, tell the user their input is not on the list. 
-
-        //Add code that stops the loop from executing once a match has been found.
-
         
 
        
 
         //Assignment Part 5
-       // Add the following to your console app and perform these actions:
-        //A list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list.
-        //Create a loop that iterates through the list and then displays the indices of the items matching the user-selected text. Ensure to remove any break statements that may prevent your code from returning multiple matches.
+      
+    
         //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list. (You do not need to provide any additional chances for the user to enter text.)
 
 
-        Console.WriteLine();
+       string[] color = ["red","orange","yellow","green","blue","purple","black","brown","gray","grey", "yellow"];
+        Console.WriteLine("Please enter your favorite color bellow:  ");
+            string usercolor = Console.ReadLine();
+            for (int n = 0; n < str1.Length; n++)
+{
+                if (color  == usercolor);
+                Console.WriteLine("Your Color is on the list" +usercolor.IndexOf(usercolor));
+                else 
+                Console.WriteLine("Your color is not on the lsit")
+}
+            
 
         //Assignment Part 6
-        //Add the following to your console app and perform these actions:
-        //Create a list of strings that has at least two identical strings in the list.
+        foreach (string z in color)
+
+        {
+            if 
+        }
         //Create a foreach loop that evaluates each item in the list, and displays a message showing the string and whether or not it has already appeared in the list. For example, if you had a list of letters: {“A”, “B”, “C”, “D”, “C”}, you could have the following print to the screen:
         //A - this item is unique
         //B - this item is unique
         //C - this item is unique
         //D - this item is unique
         //C - this item is a duplicate
-        //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-        //Upload all the code you wrote during every step of this assignment to your GitHub account and submit the link in the space below:
+       
 
         Console.WriteLine();
         }
     }
+}
 }
--- Iteration/ConsoleApp1/Program.cs
+++ Iteration/ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+
+        //int [] testScores = {98,99,85,70,82,34,91,90,94};
+
+        //for (int i =0; i < testScores.Length; i++) 
+        //{
+
+         //   if (testScores[i] > 85)
+         //   {Console.WriteLine("Passing Test Score: " + testScores[i]);
+            
+         //   }
+
+       // }
+       // Console.ReadLine();
+
+       string[] names = {"Jesse","Erik","Daniel","Adam"};
+       string[] l = 
+       for (int j = 0; j < names.Length; j++)
+       {
+            if (names[j] == "Jesse")
+                {Console.WriteLine(names[j]);
+                
+                }
+       }
+    }
+
+}
